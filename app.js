2218106e7ed717283c81a26d86462e862ffbec38
Correa Agostina Bernardette <agostina.correaok@gmail.com>
2023-08-10 07:37:44 -0300

    proyecto base updated

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const path = require('path');
 app.set('port', 3008);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine','ejs');
-//app.use(express.static('views'));
-
-const computerProducts = [{name: 'Notebook Lenovo', price: 720}, {name: 'Table Samsung 64gb', price: 230}, {name: 'MacBook Air 2020', price: 800}]
 
 //Definir ruta basica
 app.get('/',(req, res) =>{
@@ -17,23 +14,19 @@ app.get('/',(req, res) =>{
     const data = {
         title: "Sitio web",
         message: "Esto es una prueba para ver el message.",
-        products: computerProducts
     };
     res.render('index', data);
 });
 
 app.get('/cursos',(req, res) => {
-    //res.writeHead(200,{'Contect-type':'text/html'});
     res.send('<h1>Bienvenidas a nuestra seccion cursos!</h1>');
 })
 
 app.get('/contacto',(req, res) => {
-    //res.writeHead(200,{'Contect-type':'text/html'});
     res.send('<h1>Aqui puede ver nuestros datos de contacto...</h1>');
 })
 
 app.get('*',(req, res) => {
-    //res.status(404).send('No se encuentra la ruta o recurso solicitado');
     res.json({error:'404', message:'No se encuentra la ruta o recurso solicitado'});
 })
 
