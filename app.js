c0f001675f2066b6697dffe6bc9041f1e79751ae
Correa Agostina Bernardette <agostina.correaok@gmail.com>
2023-08-15 08:18:12 -0300

    json agregado con archivo .env

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+const dotenv = require('dotenv');
 const express = require('express');
-const peliculas = require('./peliculas');
+//const peliculas = require('./peliculas');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const {leerPeliculas, guardarPeliculas} = require('./src/trailerflix.manager');
 //const logger = require('morgan'); 
 
-app.set('port', 3008);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine','ejs');
+
+const PORT = process.env.PORT || 3008;
+let peliculas = [];
+
+//Inicializamos middleware
+dotenv.config();
+
+//Activamos bodyParser para que interprete en formato json
+app.use(bodyParser.json());
+
+//Middleware para guardar en la variable peliculas el contenido el trailerflix.json
+app.use((req,res,next)=>{
+    peliculas = leerPeliculas();
+    next();
+})
 
 //Definir ruta basica
 app.get('/',(req, res) =>{
@@ -20,9 +37,9 @@ app.get('/trailer/:id',(req, res) => {
     let codigo = parseInt(req.params.id);
 
     if (typeof codigo === 'number') {
-        const found = peliculas.find(element => element.codigo == codigo);
+        const found = peliculas.find(element => element.id == codigo);
 
-        found?.trailer ? res.send('ID:'+found.codigo+' Titulo: '+found.titulo+' Link al trailer:'+found.trailer):
+        found?.trailer ? res.send('ID:'+found.id+' Titulo: '+found.titulo+' Link al trailer:'+found.trailer):
         res.status(404).json({ id: 'Error', descripcion: 'El trailer de la pelicula con id = '+codigo+' no se encuentra disponible' });
         
     }
@@ -34,6 +51,6 @@ app.get('*',(req, res) => {
 })
 
 //Inicia el servidor
-app.listen(app.get('port'), ()=>{
-    console.log('Servidor iniciando en el puerto '+app.get('port')+ '...');
+app.listen(PORT, ()=>{
+    console.log('Servidor iniciando en el puerto '+PORT+' ...');
 })
